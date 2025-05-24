f947e132a49cac8f67d895342f88d1f056a96bd0
ADILOKA5758 <greenhouse322@gmail.com>
2025-05-24 15:16:22 +0800

    Update chart_api.py

--- chart_api.py
+++ chart_api.py
@@ -29,3 +29,10 @@ def chart():
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
